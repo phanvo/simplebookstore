d456439da6cc371832b2e17cee2ece7c7e51215b
Phan Vo <phanvo@gmail.com>
2020-02-12 15:22:50 -0800

    implement book class

---
+++ PV_Assign1/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PV_Assign1
+{
+    class Book
+    {
+        public string BookTitle { get; }
+        public double UnitPrice { get; }
+        public int BookCount { get; set; }
+        public double BookSubTotal
+        { 
+            get
+            {
+                return UnitPrice * BookCount;
+            }
+        }
+
+        public Book(string title, double price)
+        {
+            BookTitle = title;
+            UnitPrice = price;
+        }
+
+        public override string ToString()
+        {
+            string resultStr = "Title: {0}" +
+                               "\nUnit Price: {1}" +
+                               "\nBook Count: {2}" +
+                               "\nBook Sub Total: {3}";
+            return String.Format(resultStr, BookTitle, UnitPrice, BookCount, BookSubTotal);
+        }
+    }
+}
