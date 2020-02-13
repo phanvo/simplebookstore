26e1f68f3d42eee238cacb8cb419398e6b3a2d0b
Phan Vo <phanvo@gmail.com>
2020-02-12 16:04:44 -0800

    init main program, add mock test books

--- PV_Assign1/Program.cs
+++ PV_Assign1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace PV_Assign1
 {
     class Program
     {
+        private static void LoadBookCount(Book anyBook)
+        {
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book[] bookList = new Book[] 
+            {
+                new Book("title A", 17.3),
+                new Book("title B", 25.8),
+                new Book("title C", 39.4)
+            };
+
+            foreach (var item in bookList)
+            {
+                WriteLine(item.ToString());
+            }
+
         }
     }
 }
